21d660feb48195859bd5edbe9b7aa63734e24e4a
cranberry <jocsan.rodriguez89@stu.bmcc.cuny.edu>
2025-07-14 16:07:50 -0400

    saving

--- database/index.js
+++ database/index.js
@@ -1,11 +1,28 @@
 const db = require("./db");
 const User = require("./user");
+<<<<<<< Updated upstream
 
+=======
+const PollForm = require("./PollForm")
+>>>>>>> Stashed changes
 const Vote = require("./vote")
+
+User.hasMany(PollForm, PollForm, {foreignKey: 'poll_id'});
+PollForm.belongsTo(User);
+
+PollForm.hasMany(PollElement, {foreignKey:  'element_id'});
+pollElements.belongsTo(PollForm);
+
+
 
 module.exports = {
   db,
   User,
+<<<<<<< Updated upstream
 
+=======
+  PollForm,
+  pollElements,
+>>>>>>> Stashed changes
   Vote,
 };
