b2603d9f1e5c503502a028be2dd16052335a6673
Jocsan Rodriguez <jasub270@gmail.com>
2025-07-14 21:30:20 -0400

    Fixed relationships

--- database/index.js
+++ database/index.js
@@ -1,22 +1,32 @@
 const db = require("./db");
 const pollElements = require("./pollelements");
 const User = require("./user");
-const PollForm = require("./PollForm")
+const PollForm = require("./PollForm");
+const Vote = require("./vote");
 
-const Vote = require("./vote")
+//Associations 🎂
+
+//Create Form
+User.hasMany(PollForm, { foreignKey: "creator_id" });
+PollForm.belongsTo(User, { foreignKey: "creator_id" });
+
+PollForm.hasMany(pollElements, { foreignKey: "PollFormId" });
+pollElements.belongsTo(PollForm, { foreignKey: "PollFormId" });
+
+// For voting
+User.hasMany(Vote, { foreignKey: "user_Id" });
+Vote.belongsTo(User, { foreignKey: "user_Id" });
+
+PollForm.hasMany(Vote, { foreignKey: "PollFormId" });
+Vote.belongsTo(PollForm, { foreignKey: "PollFormId" });
+
+PollElement.hasMany(Vote, { foreignKey: "element_Id" });
+Vote.belongsTo(pollElements, { foreignKey: "element_Id" });
 
 module.exports = {
   db,
   User,
-<<<<<<< HEAD
   PollForm,
-
-
-
   pollElements,
-=======
-
   Vote,
->>>>>>> jocsan
 };
-
