2a169c6a2c80c2b2dd64f1f224dec86e18913225
Elian Echavarria <elianstanly431@gmail.com>
2025-07-15 14:13:49 -0400

    saved

--- api/pollelements.js
+++ api/pollelements.js
@@ -2,12 +2,84 @@ const express = require("express");
 const router = express.Router();
 const { pollElements } = require("../database");
 
+// get all poll elements 
 router.get("/", async (req, res) => {
     try {
-       const Pollel =  await pollElements.findAll();
-       console.log(`Found ${Pollel.length} Poll Elements`);
-       res.status(200).send("Poll Elements have been Found!")
-    } catch(err){
-        console.log("error fetching all poll elements", err)
+        const pollelements = await pollElements.findAll();
+        res.json({
+          message: "elements huh?"
+
+        });
     }
-})
+
+    catch {
+
+    }
+});
+
+// get poll elements by id
+router.get("/:id", async (req, res) => {
+
+    try {
+        const pollelement = await pollElements.findAll();
+        res.json({
+        message: "you want that one ?"
+        })
+    }
+    catch {
+ 
+    }
+
+});
+
+// patch a poll elements by id 
+router.patch("/:id", async (req, res) => {
+
+    try {
+        const pollelement = await pollElements.findAll();
+        res.json({
+        message: "remix that one "
+        })
+    }
+    catch {
+ 
+    }
+
+});
+
+
+// delete a poll elements by id
+router.delete("/:id", async (req, res) => {
+
+    try {
+        const pollelement = await pollElements.findAll();
+        res.json({
+        message: "later"
+        })
+    }
+    catch {
+ 
+    }
+
+});
+
+// create a new poll element
+router.post("/", async (req, res) => {
+
+    try {
+        const pollelement = await pollElements.findAll();
+        res.json({
+        message: "what we making ?"
+        })
+    }
+    catch {
+ 
+    }
+
+});
+
+
+
+
+
+module.exports = router;
