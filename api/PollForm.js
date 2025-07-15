6441bdcfac2e1df3a87de9497229c8a2e95fe8ba
Elian Echavarria <elianstanly431@gmail.com>
2025-07-15 15:30:39 -0400

    Added Delete route for Forms ✅

--- api/PollForm.js
+++ api/PollForm.js
@@ -5,52 +5,56 @@ const { PollForm } = require("../database");
 // get all pollforms
 router.get("/", async (req, res) => {
   try {
-    const pollforms = await PollForm.findAll();
-    res.json({
-      message: "you cooked up cuhh",
-    });
-  } catch {}
+    const pollForms = await PollForm.findAll();
+    res.status(200).send(pollForms);
+  } catch (err) {
+    res.status(500).send({ error: "Failed to get all Forms! ❌" });
+    console.error(err);
+    console.log("Failed to get all poll Forms! ❌");
+  }
 });
 
 // get pollform by id
 router.get("/:id", async (req, res) => {
   try {
-    const pollform = await PollForm.findAll();
-    res.json({
-      message: "you cooked again",
-    });
-  } catch {}
+    const pollForms = await PollForm.findAll();
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to fetch a specific Form! ❌");
+    res.status(500).send({ error: "Failed to fetch a specific task! ❌" });
+  }
 });
 
 // patch a pollform by id
 router.patch("/:id", async (req, res) => {
   try {
-    const pollform = await PollForm.findAll();
-    res.json({
-      message: "you cookin huh",
-    });
+    const pollForms = await PollForm.findAll();
   } catch {}
 });
 
 // delete a pollform by id
 router.delete("/:id", async (req, res) => {
   try {
-    const pollform = await PollForm.findAll();
-    res.json({
-      message: "pack em up",
-    });
-  } catch {}
+    const formId = await PollForm.findByPk(req.params.id);
+
+    if (!formId) {
+      return res.status(404).send("Fail to fetch specific task! ❌");
+    }
+
+    await formId.destroy();
+    res.status(200).send("Deleted Form! ✅");
+  } catch (err) {
+    console.error(err);
+    console.error("Fail to delete a specific form! ❌");
+  }
 });
 
 // create a new poll form
 router.post("/", async (req, res) => {
   try {
-    const pollform = await PollForm.findAll();
     const newForm = await PollForm.create(req.body);
+
     res.status(201).send("created a new form!✅", newForm);
-    res.json({
-      message: "create something =]",
-    });
   } catch (err) {
     console.error("Error Creating a Form! ❌");
     console.error(err);
