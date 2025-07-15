3152695adb956cbcf5b75a17874748425348c3f6
Jocsan Rodriguez <jasub270@gmail.com>
2025-07-14 21:42:04 -0400

    save

--- database/vote.js
+++ database/vote.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 const Vote = db.define("vote", {
-  id: {
+  Vote_id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
