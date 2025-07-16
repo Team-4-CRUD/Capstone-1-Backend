82836622ed76aaca26b01ce239015112255075a8
Elian Echavarria <elianstanly431@gmail.com>
2025-07-15 22:46:20 -0400

    Added Patch Route for Forms ✅

--- api/PollForm.js
+++ api/PollForm.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { PollForm, user } = require("../database");
+const { PollForm } = require("../database");
 
 // get all pollforms
 router.get("/", async (req, res) => {
@@ -34,15 +34,18 @@ router.get("/:id", async (req, res) => {
 // patch a pollform by id
 router.patch("/:id", async (req, res) => {
   try {
-    const pollform = await PollForm.findAll();
-    res.json({
-      message: "you cookin huh",
-    });
-    res.status(200).send("updated form!");
+    const pollForms = await PollForm.findByPk(req.params.id);
+
+    if (!pollForms) {
+      return res.status(404).send("Failed to update Form! ❌");
+    }
+
+    const updatedForm = await pollForms.update(req.body);
+    res.status(200).send(updatedForm);
   } catch (error) {
     console.error(error);
-    console.log("Fail to update Form!");
-    res.status(500).send({ error: "Failed to update poll form" });
+    console.log("Fail to update Form! ❌");
+    res.status(500).send({ error: "Failed to update poll form ❌" });
   }
 });
 
@@ -66,21 +69,12 @@ router.delete("/:id", async (req, res) => {
 // create a new poll form
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body);
-    const { title, description, status, creator_at, creator_id } = req.body;
-    const pollform = await PollForm.create({
-      title,
-      description,
-      status,
-      creator_at,
-      creator_id,
-    });
-    res.status(201).send(pollform);
-    console.log("Form has been created!");
+    const pollForms = await PollForm.create(req.body);
+    res.status(201).send(pollForms);
   } catch (error) {
     console.error(error);
-    console.log("Fail to created Form!");
-    res.status(500).send({ error: "Failed to create poll form" });
+    console.log("Fail to created Form! ❌");
+    res.status(500).send({ error: "Failed to create poll form ❌" });
   }
 });
 
