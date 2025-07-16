ca29568629780ce5cf8976c64650ed308d3e698c
Jocsan Rodriguez <jasub270@gmail.com>
2025-07-16 01:28:20 -0400

    added vote file

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const testDbRouter = require("./test-db");
 const pollform = require("./PollForm");
 const pollelements = require("./pollelements");
+const vote = require("./vote")
 
 router.use("/test-db", testDbRouter);
 router.use("/PollForm", pollform);
 router.use("/pollElements", pollelements);
+router.use("./vote", vote);
 
 module.exports = router;
--- api/vote.js
+++ api/vote.js
@@ -2,4 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { PollForm, pollElements } = require("../database");
 
-router.delete("/:id", )
+
+// GET /votes/pollform/:pollFormId/rank
+
+module.exports = router;
