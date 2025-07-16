3241eee2187ba2bed65e23880bb9969a727b11d0
Elian Echavarria <elianstanly431@gmail.com>
2025-07-16 10:08:15 -0400

    fix file casing

--- api/pollElements.js
+++ api/pollElements.js
@@ -2,84 +2,69 @@ const express = require("express");
 const router = express.Router();
 const { pollElements } = require("../database");
 
-// get all poll elements 
+// get all poll elements
 router.get("/", async (req, res) => {
-    try {
-        const pollelements = await pollElements.findAll();
-        res.json({
-          message: "elements huh?"
-
-        });
-    }
-
-    catch {
-
-    }
+  try {
+    const pollEl = await pollElements.findAll();
+    res.status(200).send(pollEl);
+  } catch (err) {
+    console.error(err);
+    console.error("Failed to get all Poll Elements ❌");
+    res.status(404).send("Failed to Fetch all Poll Elements ❌");
+  }
 });
 
 // get poll elements by id
 router.get("/:id", async (req, res) => {
-
-    try {
-        const pollelement = await pollElements.findAll();
-        res.json({
-        message: "you want that one ?"
-        })
-    }
-    catch {
- 
+  try {
+    const pollEl = await pollElements.findByPk(req.params.id);
+    if (!pollEl) {
+      return res.status(404).send(`${req.params.id} can't be found! 💔`);
     }
 
+    res.status(200).send(pollEl);
+  } catch (err) {
+    console.error(err);
+    console.error("Failed to Fetch a specific Poll Element! ❌📝 ");
+    res.status(404).send("Failed to Fetch a Poll Element!. ❌📝");
+  }
 });
 
-// patch a poll elements by id 
+// patch a poll elements by id
 router.patch("/:id", async (req, res) => {
-
-    try {
-        const pollelement = await pollElements.findAll();
-        res.json({
-        message: "remix that one "
-        })
-    }
-    catch {
- 
-    }
-
+  try {
+    res.json({
+      message: "Update poll element",
+    });
+  } catch {}
 });
-
 
 // delete a poll elements by id
 router.delete("/:id", async (req, res) => {
-
-    try {
-        const pollelement = await pollElements.findAll();
-        res.json({
-        message: "later"
-        })
+  try {
+    const pollEl = await pollElements.findByPk(req.params.id);
+    if (!pollEl) {
+      return res.status(404).send("Failed to delete a Poll Element! ❌");
     }
-    catch {
- 
-    }
-
+    await pollEl.destroy();
+    res.status(200).send(`Poll Element ${req.params.id} has been deleted! ✅`);
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to delete a Poll Element! ❌");
+    res.status(500).send({ error: "Failed to delete a Poll Element! ❌" });
+  }
 });
 
 // create a new poll element
 router.post("/", async (req, res) => {
-
-    try {
-        const pollelement = await pollElements.findAll();
-        res.json({
-        message: "what we making ?"
-        })
-    }
-    catch {
- 
-    }
-
+  try {
+    const pollEl = await pollElements.create(req.body);
+    res.status(201).send(pollEl);
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to create a Poll Element! 📝❌");
+    res.status(500).send({ error: "Failed to create Poll Element! 💔📝" });
+  }
 });
-
-
-
-
 
 module.exports = router;
